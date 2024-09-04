57a4915f053f379758e6a2c2f7135bf3ce563b28
Gabriel <gmarinho@ufrrj.br>
2024-09-04 18:49:37 -0300

    funções auxiliares implementadas em parte

--- auxiliares.c
+++ auxiliares.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct vetor
 {
@@ -14,7 +15,7 @@ typedef struct posicao
 
 typedef struct particula
 {
-    POSICAO posicao;
+    POSICAO coord;
     float massa;
     VETOR acelaracao;
     VETOR velocidade;
@@ -23,6 +24,13 @@ typedef struct particula
 void leParticulas(FILE arquivo)
 {
     return;
+}
+
+float calculaDistanciaR3(PARTICULA a, PARTICULA b)
+{
+    float distancia;
+    distancia = sqrt(pow(a.coord.x - b.coord.x, 2) + pow(a.coord.y - b.coord.y, 2) + pow(a.coord.z - b.coord.z, 2));
+    return distancia;
 }
 
 void atualizaAceleracao(PARTICULA a, PARTICULA b)
@@ -35,10 +43,11 @@ void atualizaVelocidade(PARTICULA a, PARTICULA b)
     return;
 }
 
-void calculaForca(PARTICULA a, PARTICULA b)
+float calculaForca(PARTICULA a, PARTICULA b)
 {
-
-    return;
+    float distancia = calculaDistanciaR3(a, b);
+    float intensidadeForca = (a.massa * b.massa * distancia) / pow(distancia, 3);
+    return intensidadeForca;
 }
 
 void atualizaParticulas(PARTICULA* particula, int numero_de_particulas)
@@ -46,9 +55,9 @@ void atualizaParticulas(PARTICULA* particula, int numero_de_particulas)
     int i, j;
     for(i=0; i<numero_de_particulas; i++)
     {
-        for(j = 0; j<numero_de_particulas; j++)
+        for(j= 0; j<numero_de_particulas; j++)
          {
-            if(j = i)
+            if(j == i)
             {
                 continue;
             }
